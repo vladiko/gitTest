0bd6825667b43c976e183879b0665f597b993ef9
vladiko <koganvladimir@yahoo.com>
2015-12-05 21:29:09 +0200

    'addint picture add usign it in the index.html'

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-window.addEventListener('load',start);
+window.addEventListener('load', start);
 
-function start(){
+function start() {
+    document.getElementById('button').addEventListener('click', click);
+    setTimeout(showPicture, 3000);
+}
 
-    document.getElementById('button').addEventListener('click',click);
+function showPicture() {
+    var picDiv = document.getElementById('picture');
+    picDiv.style.backgroundImage = "url('images/IMG_1168.JPG')";
+    picDiv.style.width = '100%';
+    picDiv.style.height = '500px';
+    picDiv.style.backgroundSize = "contain";
+    picDiv.style.backgroundRepeat = 'no-repeat';
+    picDiv.style.backgroundPosition = 'center';
+
+
+}
+
+function click() {
+    var picDiv = document.getElementById('picture');
+    picDiv.style.height = (parseInt(picDiv.style.height) + 20) + 'px';
 }
